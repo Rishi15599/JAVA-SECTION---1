ad938e9a1f250f7fef1de7dcbd0dfd265fdf59a9
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-07 20:46:11 +0530

    Switch Statement

---
+++ SwitchStatement.java
@@ -0,0 +1,25 @@
+public class SwitchStatement {
+    
+public static void main(String[] args) {
+    switch(3)
+    {
+        case 1:
+        {
+            System.out.println("Rishi");
+            break;
+
+        }
+        case 2:
+        {
+            System.out.println("Praksh");
+            break;
+        }
+        default:
+        {
+            System.out.println("Kumar");
+            break;
+        }
+    }
+    
+}
+}
