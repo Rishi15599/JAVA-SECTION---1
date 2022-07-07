b55bd3f3e55f14b2f1d83fb68c7e625d03496767
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-07 20:44:19 +0530

    Multiple Switching

--- MultipleSwitch.java
+++ MultipleSwitch.java
@@ -1,18 +1,31 @@
 public class MultipleSwitch{
     public static void main(String[] args) {
         char grade = 'Z';
-        switch('Y') {
+        switch('Y') 
+        {
             case 'X' :
+            {
             System.out.println("Good");
             break;
+            }
             case 'Y' :
+            {
             System.out.println("Well done");
             break;
+            }
             case 'Z' :
+            {
+            System.out.println("Bad");
+            break;
+
+            }
             default:
+            {
+            
             System.out.println("Very good");
+            }
         }
-        System.out.println(grade);
+       
 
         
     }
