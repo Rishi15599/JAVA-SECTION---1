a53f1d913627349959f1d83136db572e26015b3d
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-08 18:23:35 +0530

    widening conversion

---
+++ Test1.java
@@ -0,0 +1,11 @@
+public class Test1 {
+    public static void main(String[] args) {
+     int myNumber=10;
+     double d=5.5,dd;
+     dd=myNumber; //widening conversion
+     System.out.println(d);
+     System.out.println(dd);
+     System.out.println(myNumber);
+       
+    }
+}
