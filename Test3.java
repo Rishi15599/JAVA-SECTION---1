3343b846228f5f58c4b7417c6cad1fa3b0e05413
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-08 23:41:23 +0530

    Power of a number

---
+++ Test3.java
@@ -0,0 +1,14 @@
+public class Test3 {
+public static void main(String[] args) {
+
+int base =2;
+int power=10;
+int i;
+int value=1;
+
+for(i= 1;i < power; i++){
+value=value*base;
+System.out.println(value);
+}
+}
+}
