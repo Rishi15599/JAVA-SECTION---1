3a2fa050650b5daa1c87e2c8d6ddd768cad1ea39
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-08 18:38:05 +0530

    Norrowing conversion

---
+++ Test2.java
@@ -0,0 +1,10 @@
+public class Test2{
+    public static void main(String[] args) {
+     int myNumber=10;
+     double d=5.5,dd;
+     dd=myNumber; //widening conversion
+     myNumber=(int)d; //Norrowing conversion
+     System.out.println(myNumber);
+       
+    }
+}
