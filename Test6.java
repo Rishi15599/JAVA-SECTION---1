e823c68744dbf58d20622ae7f58f3839efa0bb72
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-10 10:29:02 +0530

    use of string

---
+++ Test6.java
@@ -0,0 +1,9 @@
+public class Test6 {
+    public static void main(String[] args) {
+        String first="2";
+        String second="3";
+        System.out.println(first);
+        System.out.println(second);
+    }
+}
+//use of string
