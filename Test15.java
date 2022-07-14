f4d55d8343865a8e6a9686d8f06bd284560642c2
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-14 23:48:49 +0530

    Display Numbers from 1 to 100

---
+++ Test15.java
@@ -0,0 +1,15 @@
+/*Display Numbers from 1 to 100 */
+
+
+public class Test15 {
+    public static void main(String[] args) {
+        int i = 1;
+        int n = 100;
+        while(i<=100)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
