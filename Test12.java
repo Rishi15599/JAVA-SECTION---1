5f9bb8ecb0190d92b10f4757536463af91e0f2e4
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-14 08:50:15 +0530

    TABLE(2,4,6,8,10,.....)

---
+++ Test12.java
@@ -0,0 +1,20 @@
+/* */
+
+
+public class Test12 {
+    public static void main(String[] args) {
+        
+        int a=29;
+        int i;
+        for(i=1 ;i<=10;++i)
+        {
+            
+    
+
+            System.out.println(a*i);
+
+        }
+        
+
+      }
+}
