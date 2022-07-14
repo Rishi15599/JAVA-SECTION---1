8a1b626b29da0e09d4e271e055dfd54bcca95c69
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-14 23:49:40 +0530

    Take two int values from user and print greatest

---
+++ Test14.java
@@ -0,0 +1,16 @@
+/*Take two int values from user and print greatest among them. */
+
+
+public class Test14 {
+    public static void main(String[] args) {
+        int num1=40;
+        int num2=10;
+        if(num1>num2){
+            System.out.println("num1 is Greter");
+
+        }
+        else{
+            System.out.println("num2 is Grster");
+        }
+    }
+}
