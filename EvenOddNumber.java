f1b61e9ee37eadcb84cb3bac36750983db6285f1
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-18 18:14:46 +0530

    even odd number

---
+++ EvenOddNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class EvenOddNumber {
+    public static void main(String[] args) {
+        
+        Scanner number=new Scanner(System.in);
+        System.out.println("Enter the number");
+        int i=number.nextInt();
+        if(i%2==0){
+            System.out.println("Given number "+i+" is even");
+        }
+        else{
+            System.out.println("Given number "+i+" is odd");
+        }
+
+    }
+}
