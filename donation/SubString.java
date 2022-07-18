2c928d858fd38740b694fae2584fb74ef2403fbd
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-18 18:23:35 +0530

    find out the substring in string

---
+++ donation/SubString.java
@@ -0,0 +1,12 @@
+package donation;
+
+    public class SubString {
+        public static void main(String[] args) {
+            
+        String name="Ram and shayam";
+        
+        //find out the substring in string
+
+        System.out.println(name.substring(0,4));
+        }
+    }
