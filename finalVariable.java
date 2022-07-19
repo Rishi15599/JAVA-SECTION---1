1b7c501e1b4a71716367a73520124f142255fdaa
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-19 16:19:07 +0530

    Final term are use in this program

---
+++ finalVariable.java
@@ -0,0 +1,11 @@
+
+
+public class finalVariable {
+    public static void main(String[] args) {
+        final int a=50;
+        a=a++; // not change the value because hear final variable are use
+        System.out.println(a);
+        
+    }
+    
+}
