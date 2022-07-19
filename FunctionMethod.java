7b0a3760c02977d632efe2782a9db6a0815b59f7
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-19 16:20:57 +0530

    Function Method are use

---
+++ FunctionMethod.java
@@ -0,0 +1,19 @@
+//import javax.naming.MalformedLinkException;
+
+public class FunctionMethod {
+    public static void printName(String name){
+        System.out.println("Raju");
+    }
+    public static void printAge(int a){
+        System.out.println(a);
+    }
+    public static void printGender(String Male){
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        printName("Raju");
+        printAge(50);
+         
+        
+    }
+}
