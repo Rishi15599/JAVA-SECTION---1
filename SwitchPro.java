e89e7a16ca99de77f174a6b5884f88583c55c11f
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-23 10:48:33 +0530

    Switch Statement problems

---
+++ SwitchPro.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class SwitchPro {
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        System.out.println("Enter the marks");
+        int b=a.nextInt();
+        int c=b/10;
+        switch(c)
+        {
+            case 8:{
+                System.out.println("Excelent "+c);
+            }break;
+            case 7:{
+                System.out.println("A+");
+            }break;
+            case 6:{
+                System.out.println("A");
+            }break;
+            case 5:{
+                System.out.println("B+");
+            }break;
+            case 4:{
+                System.out.println("B");
+            }break;
+            case 3:{
+                System.out.println("C");
+            }break;
+            case 2:{
+                System.out.println("D");
+            }break;
+            case 1:{
+                System.out.println("FAIL");
+            }break;
+            default:{
+                System.out.println("Your num is "+b);
+            }
+
+        }
+    }
+}
---
+++ SwitchStat.java
@@ -0,0 +1,47 @@
+//import java.util.Scanner;
+public class SwitchStat {
+    public static void main(String[] args) {
+        int a=11;
+        switch(a){
+            case 1:{
+                System.out.println("jan");
+            }break;
+            case 2:{
+                System.out.println("feb");
+            }break;
+            case 3:{
+                System.out.println("mar");
+            }break;
+            case 4:{
+                System.out.println("apr");
+            }break;
+            case 5:{
+                System.out.println("may");
+            }break;
+            case 6:{
+                System.out.println("jun");
+            }break;
+            case 7:{
+                System.out.println("jul");
+            }break;
+            case 8:{
+                System.out.println("aug");
+            }break;
+            case 9:{
+                System.out.println("sep");
+            }break;
+            case 10:{
+                System.out.println("oct");
+            }break;
+            case 11:{
+                System.out.println("nov");
+            }break;
+            case 12:{
+                System.out.println("dec");
+            }break;
+            default:{
+                System.out.println("Your number are not matching any condition"+a);
+            }
+        }
+    }
+}
