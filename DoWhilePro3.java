ea86ff5da110e5558f50e6562637943cf363fdbd
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-26 17:13:48 +0530

    Do While Problems

---
+++ DoWhilePro3.java
@@ -0,0 +1,15 @@
+//write a program table of 2 (2 * 1 = 2 like wise)
+
+public class DoWhilePro3 {
+    public static void main(String[] args) {
+        int b=1;
+        int a=2;
+        while(b<=10){
+            int c=a*b;
+            System.out.println( a + " *" + b + " =" + c);
+            b++;
+        
+        }
+        
+    }
+}
