359baf6ccea3ca28340239842cca9848acbe4cf1
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-26 17:14:37 +0530

    If else problems

---
+++ donation/IfElse.java
@@ -0,0 +1,12 @@
+
+//Write a program by using if else statement
+
+public class IfElse {
+    public static void main(String[] args) {
+        int a=50;
+        if(a==45){
+            System.out.println("a == 45");
+        }else
+        System.out.println("a != 45 "+"given number is "+a);
+    }
+}
