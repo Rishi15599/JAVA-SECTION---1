5a5990346be9b6911f3d637b4b3dc7061fc593c4
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-28 18:41:02 +0530

    For Loop Problems

---
+++ ForLoopPro.java
@@ -0,0 +1,9 @@
+public class ForLoopPro {
+    public static void main(String[] args) {
+        //int i;
+        for(int i=1;i<=10;i++){
+         System.out.println(i);
+        }
+    }
+    
+}
