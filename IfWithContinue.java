08d59b48d4809b835a04bddf313c5cf20aed5c38
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-28 18:44:23 +0530

    If with continue

---
+++ IfWithContinue.java
@@ -0,0 +1,18 @@
+
+
+public class IfWithContinue {
+    public static void main(String[] args) {
+        
+
+   
+    for(int i=2;i<10;i++){
+    //if(i == 2) {
+    
+    
+        //continue;
+      //}
+      System.out.println(i);
+    }
+}
+}
+
