7386b0ff8147d60a83ecd7ce68f57f90862bc08b
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-28 18:34:28 +0530

    Array

---
+++ Array.java
@@ -0,0 +1,11 @@
+public class Array {
+    public static void main(String[] args) {
+        int[]age={15,33,45,56,67,9};
+        System.out.println("first element " + age[0]);
+        System.out.println("second element " + age[1]);
+        System.out.println("third element " + age[2]);
+        System.out.println("fourth element " + age[3]);
+        System.out.println("fifth element " + age[4]);
+
+    }
+}
