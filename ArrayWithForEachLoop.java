63276166dba7169fa87e09ce493e438fd5d9e86c
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-28 18:36:19 +0530

    Array with for each loop

---
+++ ArrayWithForEachLoop.java
@@ -0,0 +1,10 @@
+//write a program by Using the for-each Loop
+
+public class ArrayWithForEachLoop {
+    public static void main(String[] args) {
+        int[] age={12,23,34,45,56,67,78,87};
+        for(int a:age){
+            System.out.println(a);
+        }
+    }
+}
