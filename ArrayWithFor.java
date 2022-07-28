65aa7fdd89da5c378c166331b451552c456aed84
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-28 18:34:58 +0530

    Array with for

---
+++ ArrayWithFor.java
@@ -0,0 +1,7 @@
+public class ArrayWithFor {
+    public static void main(String[] args) {
+        int[]age={67,34,87,53,90,86,53};
+        for(int i=1;i<=6;i++)
+        System.out.println("Position "+age[i]);
+    }
+}
