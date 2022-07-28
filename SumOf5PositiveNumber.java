cb5cfa29289517870805b7645adadf8f8c24f8ab
Rishi Prakash <90236666+Rishi15599@users.noreply.github.com>
2022-07-28 18:47:31 +0530

    write a program sum of positive number

---
+++ SumOf5PositiveNumber.java
@@ -0,0 +1,14 @@
+//write a program sum of positive number 
+
+public class SumOf5PositiveNumber {
+    public static void main(String[] args) {
+        int sum=0;
+        int i;
+        for(i=1;i<=6;i++){
+            sum+=i;
+        } 
+         System.out.println(sum);
+    }
+        
+    }
+
